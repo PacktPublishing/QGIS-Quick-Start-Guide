69d40c8b7a2bfd2998e3a2bfbf6c20f82207748c
Andrew Cutts <acutts@acgeospatial.co.uk>
2018-12-02 14:44:21 +0000

    Create load_layer_example.py

---
+++ Chapter_07/load_layer_example.py
@@ -0,0 +1,3 @@
+Airport_layer = iface.addVectorLayer('D:/QGIS_quickstart/qgis_sample_data/shapefiles/airports.shp','airports','ogr')
+## add another layer
+Alaska_layer = iface.addVectorLayer('D:/QGIS_quickstart/qgis_sample_data/shapefiles/alaska.shp','alaska','ogr')
